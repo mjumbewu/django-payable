97b0eee8bb2f468e5edcca90d271f8980372f0cb
Mjumbe Poe <mjumbewu@gmail.com>
2019-04-09 23:09:02 -0400

    Add 'project' field to invoices for better administration

--- src/payable/models.py
+++ src/payable/models.py
@@ -88,6 +88,7 @@ class InvoicePayment (models.Model):
 
 
 class Invoice (ClonableMixin, models.Model):
+    project = models.TextField(blank=True)
     number = models.TextField(blank=True)
     sent_date = models.DateField()
     due_date = models.DateField()
@@ -119,7 +120,7 @@ class Invoice (ClonableMixin, models.Model):
     is_paid.boolean = True
 
     def __str__(self):
-        return "Invoice for {} on {} (${:,.2f})".format(self.recipient.organization, self.sent_date, self.total_amount())
+        return "Invoice for {} to {} on {} (${:,.2f})".format(self.project or '(unknown project)', self.recipient.organization, self.sent_date, self.total_amount())
 
     def copy_relations_to(self, clone, commit=True):
         # Only copy the items, not the payments.
